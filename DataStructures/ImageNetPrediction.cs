a85a93e33c1068b95b39b75668e38ffd8e3df05b
Joao Guimaraes <jssgmrs22@gmail.com>
2020-02-06 14:05:02 +0000

    first commit

---
+++ DataStructures/ImageNetPrediction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.ML.Data;
+
+namespace ObjectDetection.DataStructures
+{
+    public class ImageNetPrediction
+    {
+        [ColumnName("grid")]
+        public float[] PredictedLabels;
+    }
+}
